b133093709620541d6e2bae87d969c1c00f85c18
tonykiselis <antanaskiselis@gmail.com>
2017-05-23 11:11:32 +0300

    First migration

---
+++ database/migrations/2017_05_23_080808_create_vr_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateVrCategoriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('vr_categories', function(Blueprint $table)
+		{
+			$table->integer('count', true);
+			$table->string('id', 36)->unique('id_UNIQUE');
+			$table->timestamps();
+			$table->softDeletes();
+			$table->string('name');
+			$table->string('comment')->nullable();
+			$table->string('language_id', 36);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('vr_categories');
+	}
+
+}
