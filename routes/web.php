b398199d782488de998acd9ab90ce17db34045eb
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-05-24 10:47:04 +0300

    add menu route and controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['prefix' => 'admin'], function () {
+
+    Route::group(['prefix' => 'menu'], function () {
+
+        Route::get('/', ['as' => 'app.menu.index', 'uses' => 'VrMenuController@index']);
+        Route::get('/create', ['as' => 'app.menu.create', 'uses' => 'VrMenuController@create']);
+        Route::post('/create', ['uses' => 'VrMenuController@store']);
+
+        Route::group(['prefix' => '{id}'], function () {
+
+            Route::get('/', ['as' => 'appmenu.show', 'uses' => 'VrMenuController@show']);
+            Route::get('/edit', ['as' => 'app.menu.edit', 'uses' => 'VrMenuController@edit']);
+            Route::post('/edit', ['uses' => 'VrMenuController@update']);
+            Route::delete('/delete', ['as' => 'app.menu.destroy', 'uses' => 'VrMenuController@destroy']);
+        });
+    });
+});
