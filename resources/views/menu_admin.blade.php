99889f39a13259420d15ba5600480a13f2898a83
Agne <agnepetrylaite@gmail.com>
2017-05-30 15:30:30 +0300

    user menu added

--- resources/views/menu_admin.blade.php
+++ resources/views/menu_admin.blade.php
@@ -4,4 +4,5 @@
     <li> <a href = {{ '/admin/categories' }}> Categories </a></li>
     <li> <a href = {{ '/admin/menu' }}> Menu </a> </li>
     <li> <a href = {{ '/admin/orders' }}> Reservations </a> </li>
+    <li> <a href = {{ '/admin/users' }}> Users </a> </li>
 </ul>
