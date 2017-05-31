b75852221065c1a10d22bc362638b94ac1f91714
tonykiselis <antanaskiselis@gmail.com>
2017-05-31 13:17:01 +0300

    Working code logic

--- resources/views/frontEnd/createOrder.blade.php
+++ resources/views/frontEnd/createOrder.blade.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-
+    {{--{{dd($checkRoomTimes)}}--}}
     @include('style')
     {{--<meta charset="utf-8">--}}
     <script src="http://code.jquery.com/jquery-2.0.0.js"></script>
@@ -15,11 +15,17 @@
                 {{$room}}
                 @foreach($dateTimeArray as $day => $hours)
                     @foreach ($hours as $hour)
+                    @if(isset($checkRoomTimes[$room]) && in_array($day.' '.$hour.':00', $checkRoomTimes[$room]))
+                        <span class="hidden">
+                            {{ Form::checkbox($key.'time[]', $day.' '.$hour, true, ['class' => 'hours']) }}
+                            {{ Form::label($day.' '.$hour, $hour)}}
+                        </span>
+                    @else
                         <span class="hidden">
                             {{ Form::checkbox($key.'time[]', $day.' '.$hour, null, ['class' => 'hours']) }}
                             {{ Form::label($day.' '.$hour, $hour)}}
                         </span>
-
+                    @endif
                     @endforeach
                 @endforeach
                 </div>
@@ -36,14 +42,14 @@
         }
         showFirst();
 
-        $('#date').on('change', hideTimes);
-           function hideTimes() {
-               $(".hours").each(function () {
-                   if ($(this).val().slice(0,-6) == $('#date').val()) {
-                       $(this).parent().removeClass('hidden');
-                   } else if(!$(this).parent().hasClass('hidden')) {
-                       $(this).parent().addClass('hidden');
-                   } else {
+                           $('#date').on('change', hideTimes);
+                       function hideTimes() {
+                           $(".hours").each(function () {
+                               if ($(this).val().slice(0,-6) == $('#date').val()) {
+                                   $(this).parent().removeClass('hidden');
+                               } else if(!$(this).parent().hasClass('hidden')) {
+                                   $(this).parent().addClass('hidden');
+                               } else {
 
                    }
                });
