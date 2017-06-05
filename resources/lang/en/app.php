9bf8bc2a37dc15151bcfde774db0483bf1d6e151
Agne <agnepetrylaite@gmail.com>
2017-06-05 15:38:21 +0300

    en translation

--- resources/lang/en/app.php
+++ resources/lang/en/app.php
@@ -3,5 +3,6 @@
 return [
     'inspired_by' => 'Inspired by:',
     'discover_vr' => 'DISCOVER VR',
-    'sentence' => 'See the world through different eyes. <br/> Virtual reality experiences show'
+    'sentence' => 'See the world through different eyes. <br/> Virtual reality experiences show',
+    'language' => 'Language'
 ];
