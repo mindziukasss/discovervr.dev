864b2918039fa68e32b4868e42a7cdf97672ffb6
Agne <agnepetrylaite@gmail.com>
2017-06-05 15:38:14 +0300

    lt translation

--- resources/lang/lt/app.php
+++ resources/lang/lt/app.php
@@ -3,5 +3,6 @@
 return [
     'inspired_by' => 'Parodą įkvepia:',
     'discover_vr' => 'ATRASK VR',
-    'sentence' => 'Pamatyk pasaulį kitomis akimis. <br/>  Virtualios realybės patirčių paroda!'
+    'sentence' => 'Pamatyk pasaulį kitomis akimis. <br/>  Virtualios realybės patirčių paroda!',
+    'language' => 'Kalba'
 ];
